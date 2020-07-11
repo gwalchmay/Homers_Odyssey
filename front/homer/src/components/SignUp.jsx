8d4877ee251292dc57293a481ade61ca10fcfa26
gwalchmay <l.luck@laposte.net>
2020-07-11 08:08:16 +0200

    quest020 done

--- front/homer/src/components/SignUp.jsx
+++ front/homer/src/components/SignUp.jsx
@@ -1,13 +1,28 @@
 import React, { useState } from 'react';
+import '../styles/SignUp.css';
 
 
 function SignUp() {
     const [email, updateEmailField] = useState('');
+    const [password, updatePasswordField] = useState('');
+    const [name, updateNameField] = useState('');
+    const [lastname, updateLastNameField] = useState('');
+    
+    function handleSubmit() {
+        console.log('email : ' + email + ', password : ' + password + ', name : ' + name + ', lastname : ' + lastname)
+    }
+    
     return (
 
-        < div >
-            <h1>{email}</h1><input type="email" name="email" onChange={(event) => updateEmailField(event.target.value)} />
-        </div >
+        <form onSubmit={() => handleSubmit()} className='flexContainer'>
+            <h1>{'email : ' + email + ', password : ' + password + ', name : ' + name + ', lastname : ' + lastname}</h1>
+            <input type="email" name="email" placeholder="example@gmail.com" onChange={(event) => updateEmailField(event.target.value)} />
+            <input type="password" name="password" placeholder="enter your password" onChange={(event) => updatePasswordField(event.target.value)} />
+            <input type="password" name="passwordbis" placeholder="enter your password again" />
+            <input type="text" name="name" placeholder="your name" onChange={(event) => updateNameField(event.target.value)} />
+            <input type="text" name="lastname" placeholder="your last name" onChange={(event) => updateLastNameField(event.target.value)} />
+            <input type="submit" value="Soumettre" />
+        </form >
     )
 }
 
