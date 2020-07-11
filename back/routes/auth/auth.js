d4decf1385c271f37963197041082b348abe28c1
gwalchmay <l.luck@laposte.net>
2020-07-11 13:02:58 +0200

    quest 040 done

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -1,9 +1,15 @@
 const router = require('express').Router();
+const connection = require('../../helpers/db.js');
 
-router.post('/signup', function (req, res, next) {
-
-    res.send('I am in POST signup');
-
+router.post('/signup', (req, res) => {
+    const formData = req.body;
+    connection.query('INSERT INTO users SET ?', formData, (err) => {
+        if (err) {
+            res.send(err.message);
+        } else {
+            res.sendStatus(200);
+        }
+    });
 });
 
 module.exports = router;
