ef0b06ae731f2f5ac00e5e4a002dab1def24e2d8
gwalchmay <l.luck@laposte.net>
2020-07-11 15:36:57 +0200

    quest 060 done

--- front/homer/src/components/SignUp.jsx
+++ front/homer/src/components/SignUp.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
 import '../styles/SignUp.css';
+import TextField from '@material-ui/core/TextField';
+import { Button, Snackbar } from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 
 
 function SignUp() {
@@ -8,6 +12,19 @@ function SignUp() {
     const [name, updateNameField] = useState('');
     const [lastname, updateLastNameField] = useState('');
     const [flash, setFlash] = useState('')
+    const [open, setOpen] = React.useState(false);
+
+    const handleClick = () => {
+        setOpen(true);
+    };
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpen(false);
+    };
     function handleSubmit(event) {
         event.preventDefault();
         fetch("http://localhost:8000/auth/signup",
@@ -22,24 +39,38 @@ function SignUp() {
             .then(
                 res => setFlash(res.flash),
                 err => setFlash(err.flash)
-            )
+            ).then(handleClick())
 
     }
 
     return (
-        <div>
+        <div className='signUp_rootDiv'>
             <form onSubmit={(event) => handleSubmit(event)} className='flexContainer'>
-                <h1>{'email : ' + email + ', password : ' + password + ', name : ' + name + ', lastname : ' + lastname}</h1>
-                <input type="email" name="email" placeholder="example@gmail.com" onChange={(event) => updateEmailField(event.target.value)} />
-                <input type="password" name="password" placeholder="enter your password" onChange={(event) => updatePasswordField(event.target.value)} />
-                <input type="password" name="passwordbis" placeholder="enter your password again" />
-                <input type="text" name="name" placeholder="your name" onChange={(event) => updateNameField(event.target.value)} />
-                <input type="text" name="lastname" placeholder="your last name" onChange={(event) => updateLastNameField(event.target.value)} />
-                <input type="submit" value="Soumettre" />
+                <h1>SIGN UP!</h1>
+                <TextField type="email" name="email" label='email' placeholder="example@gmail.com" onChange={(event) => updateEmailField(event.target.value)} />
+                <TextField type="password" name="password" label='password' placeholder="enter your password" onChange={(event) => updatePasswordField(event.target.value)} />
+                <TextField type="password" name="passwordbis" label='password again' placeholder="enter your password again" />
+                <TextField type="text" name="name" label='name' placeholder="your name" onChange={(event) => updateNameField(event.target.value)} />
+                <TextField type="text" name="lastname" label='lastname' placeholder="your last name" onChange={(event) => updateLastNameField(event.target.value)} />
             </form >
-            <div>
-                {flash}
-            </div>
+                <Button type="submit" variant="contained">Submit</Button>
+            <Snackbar
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                }}
+                open={open}
+                autoHideDuration={6000}
+                onClose={handleClose}
+                message={flash}
+                action={
+                    <React.Fragment>
+                        <IconButton size="small" aria-label="close" color="inherit" onClick={handleClose}>
+                            <CloseIcon fontSize="small" />
+                        </IconButton>
+                    </React.Fragment>
+                }
+            />
         </div>
     )
 }
