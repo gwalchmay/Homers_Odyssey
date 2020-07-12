9f691176c3f80c3b8f84291f66987aeeeb33b5b9
gwalchmay <l.luck@laposte.net>
2020-07-12 17:49:17 +0200

    quest 080 done

--- back/App.js
+++ back/App.js
@@ -4,9 +4,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
+const connection = require('./helpers/db.js');
 const authRouter = require('./routes/auth/auth.js');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const JWTStrategy = require('passport-jwt').Strategy;
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 require('dotenv').config();
+const cors = require('cors');
+const ExtractJWT = require('passport-jwt').ExtractJwt;
 
+
+const corsOptions = {
+    origin: process.env.CLIENT_APP_ORIGIN,
+};
+app.use(cors(corsOptions));
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -16,6 +29,11 @@ app.use('/auth', authRouter);
 app.get("/", (req, res) => {
     res.send("youhou");
 })
+
+app.get("/profile", passport.authenticate('jwt', { session: false }), function (req, res) {
+    res.send(req.user);
+})
+
 /// dans le cas d'une route non trouvée, je retourne le code 404 'Not Found'
 app.use(function (req, res, next) {
     var err = new Error('Not Found');
@@ -27,3 +45,39 @@ app.use(function (req, res, next) {
 let server = app.listen(process.env.PORT || 3000, function () {
     console.log('Listening on port ' + server.address().port);
 });
+
+passport.use(new LocalStrategy(
+    {
+        usernameField: 'email',
+        passwordField: 'password',
+        session: false
+    },
+    function (email, password, done) {
+        //Your code here
+        connection.query(`SELECT * FROM users WHERE email = "${email}" `, function (err, res) {
+            if (err) {
+                return done(err);
+            }
+            else if (!res.length) {
+                return done(null, false, { info: 'Unknown Email' });
+            }
+            else if (!bcrypt.compareSync(password, res[0].password)) {
+                return done(null, false, { info: 'Wrong Password' })
+            }
+            else {
+                return done(null, res[0], { info: 'User Signed in' })
+            }
+        });
+    }
+));
+
+passport.use(new JWTStrategy({
+
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.ACCESS_TOKEN_SECRET
+},
+
+    function (jwtPayload, cb) {
+        return cb(null, jwtPayload);
+    }
+));
--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -1,15 +1,30 @@
 const router = require('express').Router();
 const connection = require('../../helpers/db.js');
+require('dotenv').config();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+var bcrypt = require('bcrypt');
 
 router.post('/signup', (req, res) => {
-    const formData = req.body;
+    let hash = bcrypt.hashSync(req.body.password, 10);
+    const formData = { 'email': req.body.email, 'password': hash, 'name': req.body.name, 'lastname': req.body.lastname }
     connection.query('INSERT INTO users SET ?', formData, (err) => {
-        if (err) {
-            res.send(err.message);
-        } else {
-            res.sendStatus(200);
-        }
+        if (err)
+            res.status(500).json({ flash: err.message });
+        else
+            res.status(200).json({ flash: "User has been signed up !" });
     });
 });
 
+router.post('/signin', function (req, res) {
+
+    passport.authenticate('local', (err, user, info) => {
+        if (err) return res.status(500).send(err)
+        if (!user) return res.status(400).json({ message: info.message });
+        const token = jwt.sign(JSON.stringify(user), process.env.ACCESS_TOKEN_SECRET);
+        return res.json({ user, token });
+    })(req, res);
+});
+
 module.exports = router;
+
